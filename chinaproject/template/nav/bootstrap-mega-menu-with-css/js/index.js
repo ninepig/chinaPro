30fa2d5a0359664bb231e91fe02c436e46c8d83c
wenjing yang <yangwenjingshogun@gmail.com>
2016-05-27 14:22:36 -0400

    meeting using secondtime

--- chinaproject/template/nav/bootstrap-mega-menu-with-css/js/index.js
+++
@@ -1,9 +0,0 @@
-// Dropdown Menu Fade    
-jQuery(document).ready(function(){
-    $(".dropdown").hover(
-        function() { $('.dropdown-menu', this).fadeIn("fast");
-        },
-        function() { $('.dropdown-menu', this).fadeOut("fast");
-    });
-
-});
